fd8a2a976e23c97da9335ee489ea4a3c646e9c51
Sayuti Daniel <sayuti@traveloka.com>
2017-06-09 21:39:00 +0700

    Expose soya-form public API

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import createForm from './createForm';
+import createField from './createField';
+import createRepeatable from './createRepeatable';
+import withForm from './withForm';
+
+export default {
+  createForm,
+  createField,
+  createRepeatable,
+  withForm,
+};
