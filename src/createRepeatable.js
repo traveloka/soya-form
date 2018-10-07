1e62f7dc1e9e17296bebc03cb6c85783b90cc0d8
fawwaz <fawwaz.muhammad@traveloka.com>
2018-10-07 14:56:40 +0700

    Create Repeatable Action

--- src/createRepeatable.js
+++ src/createRepeatable.js
@@ -1,54 +1,81 @@
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { applyReducers } from 'soya-next/redux';
-import reducers from './_reducers';
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { applyReducers } from "soya-next/redux";
+import reducers from "./_reducers";
 import {
   addListItem,
   removeListItem,
   reorderListItem,
   reorderListItemDec,
-  reorderListItemInc,
-} from './_actions';
-import createSelector from './_selectors';
-import {
-  getFieldNames,
-} from './_utils';
-import withForm from './withForm';
-import _createRepeatable from './_createRepeatable';
+  reorderListItemInc
+} from "./_actions";
+import createSelector from "./_selectors";
+import { getFieldNames } from "./_utils";
+import withForm from "./withForm";
+import _createRepeatable from "./_createRepeatable";
 
 const mapStateToProps = (state, props) => {
   const fieldNames = getFieldNames(props.name);
   const selector = createSelector(state, props.form.getFormId());
   const length = selector.getLength(fieldNames);
-  const minLength = (typeof props.minLength === 'number') && props.minLength >= 0 ? props.minLength : 1;
+  const minLength =
+    typeof props.minLength === "number" && props.minLength >= 0
+      ? props.minLength
+      : 1;
 
   return {
     length: length >= minLength ? length : minLength,
-    name: fieldNames,
+    name: fieldNames
   };
 };
 
 const mapDispatchToProps = (dispatch, props) => ({
   addListItem: fieldNames => () => {
-    dispatch(addListItem(props.form.getFormId(), fieldNames, props.minLength, props.maxLength));
+    dispatch(
+      addListItem(
+        props.form.getFormId(),
+        fieldNames,
+        props.minLength,
+        props.maxLength
+      )
+    );
   },
   removeListItem: fieldNames => index => {
     dispatch(removeListItem(props.form.getFormId(), fieldNames, index));
   },
   reorderListItem: fieldNames => (index, targetIndex) => {
-    dispatch(reorderListItem(props.form.getFormId(), fieldNames, index, targetIndex));
+    dispatch(
+      reorderListItem(props.form.getFormId(), fieldNames, index, targetIndex)
+    );
   },
   reorderListItemDec: fieldNames => (index, amount) => {
-    dispatch(reorderListItemDec(props.form.getFormId(), fieldNames, index, amount));
+    dispatch(
+      reorderListItemDec(props.form.getFormId(), fieldNames, index, amount)
+    );
   },
   reorderListItemInc: fieldNames => (index, amount) => {
-    dispatch(reorderListItemInc(props.form.getFormId(), fieldNames, index, amount));
-  },
+    dispatch(
+      reorderListItemInc(props.form.getFormId(), fieldNames, index, amount)
+    );
+  }
 });
+
+export const createRepeatableAction = Component =>
+  compose(
+    withForm,
+    applyReducers(reducers),
+    connect(
+      mapStateToProps,
+      mapDispatchToProps
+    )
+  )(Component);
 
 export default compose(
   withForm,
   applyReducers(reducers),
-  connect(mapStateToProps, mapDispatchToProps),
-  _createRepeatable,
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  _createRepeatable
 );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import createForm from './createForm';
-import createField from './createField';
-import createRepeatable from './createRepeatable';
-import createSelector from './_selectors';
-import withForm from './withForm';
-import withFormValues from './withFormValues';
+import createForm from "./createForm";
+import createField from "./createField";
+import createRepeatable from "./createRepeatable";
+import { createRepeatableAction } from "./createRepeatable";
+import createSelector from "./_selectors";
+import withForm from "./withForm";
+import withFormValues from "./withFormValues";
 
 export {
   createForm,
   createField,
   createRepeatable,
+  createRepeatableAction,
   createSelector,
   withForm,
-  withFormValues,
+  withFormValues
 };
