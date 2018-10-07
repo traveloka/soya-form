fea70da51467a24a52e421bac817b1f36d654b4e
fawwaz <fawwaz.muhammad@traveloka.com>
2018-10-07 15:05:53 +0700

    [FEATURE] Add Repeatable action HOC

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import createForm from "./createForm";
 import createField from "./createField";
-import createRepeatable from "./createRepeatable";
-import { createRepeatableAction } from "./createRepeatable";
+import createRepeatable, { createRepeatableAction } from "./createRepeatable";
 import createSelector from "./_selectors";
 import withForm from "./withForm";
 import withFormValues from "./withFormValues";
